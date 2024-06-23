b1b5f9791cd740efb854b37a22c4a9e1285a7df2
huzongyao <huzongyao@tangbull.com>
2024-06-23 08:37:09 +0800

    refine clock

--- ESP8266-OLED-Clock/src/main.cpp
+++ ESP8266-OLED-Clock/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 #include <ESP8266WiFi.h>
 #include <U8g2lib.h>
 
@@ -51,6 +52,35 @@ void inline startConfigTime() {
   delay(500);
 }
 
+void inline setupOTAConfig() {
+  ArduinoOTA.onStart([] {});
+  ArduinoOTA.onProgress([](u32_t pro, u32_t total) {
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_unifont_t_chinese3);
+    u8g2.setCursor(0, 14);
+    u8g2.print("OTA升级中...");
+    u8g2.setCursor(0, 30);
+    int per = pro * 100 / total;
+    sprintf(buf, "升级进度: %d%%", per);
+    u8g2.print(buf);
+    u8g2.drawRFrame(0, 40, 104, 14, 3);
+    if (per > 2) {
+      u8g2.drawRBox(2, 42, per, 10, 1);
+    }
+    u8g2.sendBuffer();
+  });
+  ArduinoOTA.onEnd([] {
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_unifont_t_chinese3);
+    u8g2.setCursor(0, 14);
+    u8g2.print("OTA升级成功");
+    u8g2.setCursor(0, 30);
+    u8g2.print("Rebooting...");
+    u8g2.sendBuffer();
+  });
+  ArduinoOTA.begin();
+}
+
 void setup() {
   Serial.begin(115200);
   u8g2.begin();
@@ -63,28 +93,32 @@ void setup() {
   startWifiConfig();
   startConfigTime();
   ip = WiFi.localIP().toString();
+  setupOTAConfig();
 }
 
 void inline showCurrentTime() {
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_unifont_t_chinese1);
-  u8g2.setCursor(0, 14);
-  u8g2.print(ip.c_str());
   struct tm info;
   getLocalTime(&info);
+  u8g2.clearBuffer();
+  // Time
   strftime(buf, 32, "%T", &info);
   u8g2.setFont(u8g2_font_logisoso24_tr);
   u8g2.setCursor(0, 44);
   u8g2.print(buf);
+  // Date
   sprintf(buf, "%04d-%02d-%02d 周%s", 1900 + info.tm_year, info.tm_mon + 1,
           info.tm_mday, WEEK_DAYS[info.tm_wday]);
-  u8g2.setCursor(0, 61);
+  u8g2.setCursor(0, 14);
   u8g2.setFont(u8g2_font_unifont_t_chinese3);
   u8g2.print(buf);
+  // IP
+  u8g2.setCursor(0, 61);
+  u8g2.print(ip.c_str());
   u8g2.sendBuffer();
 }
 
 void loop() {
   delay(300);
   showCurrentTime();
+  ArduinoOTA.handle();
 }
